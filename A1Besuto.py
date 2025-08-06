f4f8c82b51d84bfd55531f5925255a04ead50bb8
Rapepong Pitijaroonpong <besutodesuka@gmail.com>
2025-08-07 01:07:20 +0700

    can go straight to target

--- A1Besuto.py
+++ A1Besuto.py
@@ -59,22 +59,28 @@ class GreedyWalkRobot(Robot):
         # determine quardrant of target
         block_area = select_with_index(surrounding_dist, front_sensor)
         # print(block_area)        
-        if any([i <= 10 for i in  block_area]) :
+        if any([i <= 10 for i in block_area]) :
             avg_dist_left = (surrounding_dist[7] + surrounding_dist[6])/2
             avg_dist_right = (surrounding_dist[1] + surrounding_dist[2])/2
             if avg_dist_left < 10:
                 self.turn(30)
+                print("l")
             elif avg_dist_right < 10:
                 self.turn(-30)
+                print("r")
             else:
                 self.turn(15)
+                print("not sure")
+            surrounding_dist = self.distance()
+            block_area = select_with_index(surrounding_dist, front_sensor)
             if sum(block_area)/len(block_area) < 10:
-                self.turn(180)
-                self.move(10)
+                self.turn(90)
             if surrounding_dist[0] > 20:
                 self.move(10)
         else:
             self.move(3)
+        surrounding_dist = self.distance()
+        block_area = select_with_index(surrounding_dist, front_sensor)
         if sum(block_area)/len(block_area) > 70:
             self.turn(direction)
         surrounding_dist = self.distance()
