086ca8c02a5483b125b57f124a7fdd774d1c5618
Rapepong Pitijaroonpong <besutodesuka@gmail.com>
2025-08-26 22:36:58 +0700

    fuzzy logic rework

--- Assign2/A2besuto.py
+++ Assign2/A2besuto.py
@@ -24,21 +24,6 @@ START_POINT = (20, 560)
 # Map file
 MAP_FILE = 'maps/default_map.kv'
 
-# def get_adjacent(l: list, n: int, k: int = 2):
-#     temp = [l[n]]
-#     for i in range(1,k+1):
-#         temp.append(l[(n+i)%8])
-#         temp.append(l[(n-i)%8])
-#     return temp
-
-# def mf_free(self, target, threadshold = 0.5):
-#     # filter_ir = get_adjacent(self.ir_values, n = target, k = 1)
-#     filter_ir = self.ir_values
-#     degree = sum(filter_ir)/len(filter_ir)
-#     if degree < threadshold:
-#         return 0
-#     return  ((degree / 100) - threadshold)/(1-threadshold)
-
 class FuzzyRobot(Robot):
 
     def __init__(self):
@@ -56,75 +41,55 @@ class FuzzyRobot(Robot):
         turns = list()
         moves = list()
 
-        # go forward base on front distance
-        rules.append(self.mf_far(0))
-        turns.append(0)
+        # 1. if front (0,1,7) are far and smell on the left **then** move moderately(5) and turn left (-30)
+        # 2. if front (0,1,7) are far and smell on the right **then** move moderately(5) and turn right(-30)
+        # ***(1-2 imply to straight line toward the target)***
+        rules.append(self.mf_far(0) * self.mf_far(1) * self.mf_far(7) * self.mf_target('l'))
+        turns.append(-30)
         moves.append(5)
 
-        # go forward base on front distance toward food
-        target_sensor = math.floor(self.target/45)
-        rules.append(max(
-             self.mf_approaching(0),
-            0
-            # self.mf_near(0)
-              )
-              )
-        turns.append(self.target)
+        rules.append(self.mf_far(0) * self.mf_far(1) * self.mf_far(7) * self.mf_target('r'))
+        turns.append(30)
+        moves.append(5)
+
+        # 3. if  left rear (6) is near **then turn** (10) a bit to right side
+        # 4. if  right rear (2) is near **then turn** (-10) a bit to left side 
+        # ***(3-4 avoiding turning straight to the wall while move in parallel more like adjusting position so no need to move)***
+        rules.append(self.mf_near(6))
+        turns.append(10)
+        moves.append(1)
+
+        rules.append(self.mf_near(2))
+        turns.append(-10)
+        moves.append(1)
+
+        # 5. if front is near (0) but front left (7) is far (left have space) and smell left then turn left (-30) and move a little (2) to turn
+        # 6. if front is near (0) but front right (1) is far (right have space)  and smell right then turn right (30) and move a little (2) to turn
+        # ***(5-6 avoid incoming wall which if  there is no available  space in 45 degree direction then it will cancel out and  it will utilize other policy***
+        rules.append(self.mf_near(0) * self.mf_far(7))
+        turns.append(-60) # favor left side if the dilemma appear
+        moves.append(3)
+
+        rules.append(self.mf_near(0) * self.mf_far(1))
+        turns.append(30)
+        moves.append(3)
+
+        # 7. if right rear is far then turn 45 and move 2
+        # 8. if left rear is far then turn -40 and move 2
+        # (7-8 is making model favor turning  to blank  space)
+        rules.append(self.mf_far(6) * self.mf_far(7))
+        turns.append(-40)
         moves.append(2)
-        
 
-        # avoid left
-        rules.append(max(
-            self.mf_near(6),
-            self.mf_near(7),
-            # self.mf_near(5)
-                  ))
-        turns.append(45)
-        moves.append(3)
+        rules.append(self.mf_far(2) * self.mf_far(1))
+        turns.append(40)
+        moves.append(2)
 
-         # avoid right
-        rules.append(max(
-            self.mf_near(2),
-            self.mf_near(1),
-            # self.mf_near(3)
-                  ))
-        turns.append(-45)
-        moves.append(3)
-        # rules.append(
-        #     self.mf_near(0)
-        #       )
-        # turns.append(90)
-        # moves.append(-2)
+        #if front is so clear get speed boost
+        rules.append(self.mf_far(0))
+        turns.append(0)
+        moves.append(2)
 
-        # rules.append(
-        #     self.mf_far(math.floor(self.target/45))
-        #           )
-        # turns.append(self.target)
-        # moves.append(1)
-
-
-        # # # turn left if food is left
-        # rules.append(self.mf_target('l')*self.mf_far(1)*self.mf_far(2)*self.mf_far(3))
-        # turns.append(self.target)
-        # moves.append(3)
-
-        # # # turn left if food is right
-        # rules.append(self.mf_target('r')*self.mf_far(7)*self.mf_far(6)*self.mf_far(5))
-        # turns.append(self.target)
-        # moves.append(3)
-
-        # # # turn back if food is back
-        # rules.append(self.mf_target('b')*self.mf_far(3)*self.mf_far(4)*self.mf_far(5))
-        # turns.append(self.target)
-        # moves.append(3)
-
-        # escape from obstrucle
-        # for i, v in enumerate(self.ir_values):
-        #     if v == max(self.ir_values):
-        #         rules.append(self.mf_near(i)*(self.mf_near(0) * self.mf_near(1) * self.mf_near(7)))
-        #         turns.append(45 * (i-4))
-        #         moves.append(10)
-        
         ans_turn = 0.0
         ans_move = 0.0
         for r, t, m in zip(rules, turns, moves):
@@ -135,17 +100,11 @@ class FuzzyRobot(Robot):
 
         self.turn(ans_turn)
         self.move(ans_move)
-
-    def mf_free(self, threadshold = 0.5):
-        degree = sum(self.ir_values)/len(self.ir_values)
-        if degree < threadshold:
-            return 0
-        return  ((degree / 100) - threadshold)/(1-threadshold)
         
     def mf_far(self, ir):
         distance = self.ir_values[ir]
         lower_bound = 5
-        higher_bound = 25
+        higher_bound = 20
         if distance>=higher_bound:
             return 1
         elif distance<lower_bound:
@@ -155,8 +114,8 @@ class FuzzyRobot(Robot):
         
     def mf_near(self, ir):
         distance = self.ir_values[ir]
-        lower_bound = 5
-        higher_bound = 20
+        lower_bound = 10
+        higher_bound = 25
         if distance>=higher_bound:
             return 0
         elif distance<lower_bound:
@@ -164,40 +123,14 @@ class FuzzyRobot(Robot):
         else:
             return (higher_bound - distance)/abs(higher_bound - lower_bound)
         
-        
-    def mf_approaching(self, ir):
-        distance = self.ir_values[ir]
-        lower_bound = 60
-        higher_bound = 30
-        if distance>lower_bound:
-            return 0
-        elif distance<higher_bound:
-            return 1
-        else:
-            return (lower_bound - distance)/abs(higher_bound - lower_bound)
-        
     def mf_target(self, mode):
-        # mode: ('f','l','b','r')
-        #  at front
-        if self.target > 315 or self.target <= 45:
-            if mode == 'f':
-                # calculate
-                if self.target > 315:
-                    return (self.target-315)/90
-                else:
-                    return self.target/90
+        # mode: ('l','r')
+        if self.target < 180 and mode == 'r':
+            return self.target/180
+        elif self.target >= 180 and  mode == 'l':
+            return (self.target-180)/180
         else:
-            lower_bound = 45
-            higher_bound = 135
-            if mode == 'b':
-                lower_bound+=90
-                higher_bound+=90
-            if mode == 'l':
-                lower_bound+=180
-                higher_bound+=180
-            if self.target > lower_bound and self.target <= higher_bound:
-                return (self.target-lower_bound)/90
-        return 0
+            return 0
 
 if __name__ == '__main__':
     app = PySimbotApp(FuzzyRobot, ROBOT_NUM, mapPath=MAP_FILE, interval=TIME_INTERVAL, maxtick=MAX_TICK)
