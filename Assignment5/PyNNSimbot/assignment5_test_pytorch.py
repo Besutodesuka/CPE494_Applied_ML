fix: feature scaling

Assignment5/PyNNSimbot/assignment5_test_pytorch.py
@@ -5,6 +5,7 @@ from kivy.logger import Logger
 from kivy.config import Config
 import torch
 import torch.nn as nn
+from ann import Net
 import random
 import numpy as np
 
@@ -19,20 +20,6 @@ def scale(data, from_interval: Tuple[float, float], to_interval: Tuple[float, fl
     to_min, to_max = to_interval
     scaled_data = to_min + (data - from_min) * (to_max - to_min) / (from_max - from_min)
     return scaled_data
-
-# Define the PyTorch model
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(9, 32)
-        self.fc2 = nn.Linear(32, 16)
-        self.fc3 = nn.Linear(16, 2)
-
-    def forward(self, x):
-        x = torch.sigmoid(self.fc1(x))
-        x = torch.sigmoid(self.fc2(x))
-        x = torch.sigmoid(self.fc3(x))
-        return x
 
 # 2. create robot for testing the model.
 class NNRobot(Robot):
@@ -68,18 +55,12 @@ class NNRobot(Robot):
         output = output.numpy().flatten()
 
         # scale the output back
-        answerTurn = scale(output[0], (0, 1), (-90, 90))
-        answerMove = scale(output[1], (0, 1), (-10, 10))
+        answerTurn = scale(output[1], (0, 1), (-180, 180))
+        answerMove = scale(output[0], (0, 1), (-10, 10))
 
         # perform the robot movement
         self.turn(int(answerTurn))
         self.move(answerMove)
-
-        if self.stuck:
-            Deg = random.randint(-10, 10)
-            self.turn(Deg)
-            self.move(-5)
-
 
 # 3. start the simulation
 if __name__ == '__main__':

Assignment5/PyNNSimbot/assignment5_train_pytorch.py
@@ -4,7 +4,9 @@ import pandas as pd
 from typing import Tuple
 import matplotlib.pyplot as plt
 import os
-
+import logging
+from ann import Net
+logger = logging.getLogger()
 # 1. define scaling function
 def scale(data, from_interval: Tuple[float, float], to_interval: Tuple[float, float]=(0, 1)):
     from_min, from_max = from_interval
@@ -14,48 +16,39 @@ def scale(data, from_interval: Tuple[float, float], to_interval: Tuple[float, fl
 
 # 2. read data
 dataframe = pd.read_csv('history_all.csv', sep=',')
-dataframe.iloc[:, 0:8] = scale(dataframe.iloc[:, 0:8], from_interval=(0, 100), to_interval=(0,1))
-dataframe.iloc[:, 8] = scale(dataframe.iloc[:, 8], from_interval=(-180, 180), to_interval=(0,1))
-dataframe.iloc[:, 9:] = scale(dataframe.iloc[:, 9:], from_interval=(-5, 5), to_interval=(0,1))
+dataframe.iloc[:, 0:8] = scale(dataframe.iloc[:, 0:8], from_interval=(0, 100), to_interval=(0,1)) # distance
+dataframe.iloc[:, 8] = scale(dataframe.iloc[:, 8], from_interval=(-180, 180), to_interval=(0,1)) # scale smell feature
+dataframe.iloc[:, 9] = scale(dataframe.iloc[:, 9], from_interval=(-10, 10), to_interval=(0,1))
+dataframe.iloc[:, 10] = scale(dataframe.iloc[:, 10], from_interval=(-180, 180), to_interval=(0,1))
 
 # 3. select input and output of the ANN
 x = dataframe.iloc[:, :9].values
 y = dataframe.iloc[:, 9:].values
 
+print(x, y)
+
 # Convert to PyTorch tensors
 X_tensor = torch.tensor(x, dtype=torch.float32)
 y_tensor = torch.tensor(y, dtype=torch.float32)
-
 # 4. define ANN architecture, loss and optimizer
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(9, 32)
-        self.fc2 = nn.Linear(32, 16)
-        self.fc3 = nn.Linear(16, 2)
-
-    def forward(self, x):
-        x = torch.sigmoid(self.fc1(x))
-        x = torch.sigmoid(self.fc2(x))
-        x = torch.sigmoid(self.fc3(x))
-        return x
-
 model = Net()
 if os.path.isfile('assignment5_model.pth'):
     model.load_state_dict(torch.load('assignment5_model.pth'))
-    print("Loaded pre-trained model.")
+    logger.info("Loaded pre-trained model.")
 
 
 
 criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 # 5. train ANN model
 epochs = 1000
-batch_size = 1000
+batch_size = 1024
 losses = []
 
-for epoch in range(epochs):
+from tqdm import tqdm
+
+for epoch in tqdm(range(epochs)):
     for i in range(0, len(X_tensor), batch_size):
         # Get batch
         X_batch = X_tensor[i:i+batch_size]
@@ -72,12 +65,12 @@ for epoch in range(epochs):
 
     losses.append(loss.item())
     if (epoch+1) % 100 == 0:
-        print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+        logger.info(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
 
 
 # 6. save model for later usage
 torch.save(model.state_dict(), 'assignment5_model.pth')
-print("Saved trained model to assignment5_model.pth")
+logger.info("Saved trained model to assignment5_model.pth")
 
 # 7. plot the loss value of the training
 plt.plot(losses)